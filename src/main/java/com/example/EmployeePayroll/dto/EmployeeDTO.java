ee5b99b9928b4233ee13d4d364539f50189816ad
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-07 02:54:19 +0530

    Introducing DTO and Model in Employee Payroll App

---
+++ src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
@@ -0,0 +1,14 @@
+package com.example.EmployeePayroll.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+}
