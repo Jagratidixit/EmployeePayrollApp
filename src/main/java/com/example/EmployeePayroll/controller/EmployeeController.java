55a6ffd038bdfb59acce40f4b348ad49878cc6fb
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-08 11:57:23 +0530

    Use Lombok Library for Logging

--- src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
@@ -1,35 +1,42 @@
 package com.example.EmployeePayroll.controller;
-
 import com.example.EmployeePayroll.dto.EmployeeDTO;
 import com.example.EmployeePayroll.model.Employee;
 import com.example.EmployeePayroll.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
+@Slf4j //logging
 public class EmployeeController {
+
     @Autowired
     private EmployeeService employeeService;
 
     @PostMapping("/create")
     public Employee createUser(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("Creating Employee: {}", employeeDTO);
         return employeeService.createUser(employeeDTO);
     }
 
     @GetMapping("/")
     public List<Employee> getAllUsers() {
+        log.info("Fetching all employees");
         return employeeService.getAllUsers();
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<Employee> getUserById(@PathVariable Long id) {
+        log.info("Fetching employee with ID: {}", id);
         return employeeService.getUserById(id)
                 .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .orElseGet(() -> {
+                    log.warn("Employee with ID {} not found", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 }
